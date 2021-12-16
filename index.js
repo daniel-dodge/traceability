d05065a14c52a1f719d64b3cb137fb52f48c6be4
Daniel Dodge <dandantheman87@gmail.com>
2021-12-16 14:11:49 -0700

    working js

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/public/index.html"))
+})
 const port = process.env.PORT || 4545
 app.listen(port, () => console.log(`port ${port} up`))
