467e78dcfb75503a97790c9836a392e027042feb
Daniel Dodge <dandantheman87@gmail.com>
2021-12-16 14:21:57 -0700

    updated index.js and package.json

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const path = require('path')
+
 const Rollbar = require('rollbar')
 const rollbar = new Rollbar({
   accessToken: '61e8c1e70f5b40c3ba32e5afa250c284',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
+
 const app = express()
+app.use(express.json)
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,"/public/index.html"))
 })
+
+app.use(rollbar.errorHandler())
 rollbar.log('hello world')
 const port = process.env.PORT || 4545
 app.listen(port, () => console.log(`port ${port} up`))
